bed3fef81ae6de26e30824b6df9e3dc00886f871
22026526-san <sangv6548@gmail.com>
2025-07-18 11:36:55 +0700

    Delete comment

--- mobile/App.js
+++ mobile/App.js
@@ -1,25 +1,3 @@
-// import { StatusBar } from 'expo-status-bar';
-// import { StyleSheet, Text, View } from 'react-native';
-
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <Text>Hello San</Text>
-//       <StatusBar style="auto" />
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
-
-// App.js
 import React from 'react';
 import { ClerkProvider } from '@clerk/clerk-expo';
 import AppNavigator from './navigations/AppNavigator'; 
