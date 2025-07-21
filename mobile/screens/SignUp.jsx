81dbf92a449f30db60117ba8d45ea8a2a942770e
22026526-san <sangv6548@gmail.com>
2025-07-21 15:32:46 +0700

    fe signUp and Verification OTP

--- mobile/screens/SignUp.jsx
+++ mobile/screens/SignUp.jsx
@@ -1,32 +1,42 @@
-import React from 'react'
-import { Text, TextInput, TouchableOpacity, View } from 'react-native'
+import React, { useRef, useState, useEffect } from 'react'
+import { Text, TextInput, TouchableOpacity, View, StyleSheet, KeyboardAvoidingView, Platform, ScrollView } from 'react-native'
 import { useSignUp } from '@clerk/clerk-expo'
-import { Link, useRouter } from 'expo-router'
+import { Ionicons } from '@expo/vector-icons';
 
 export default function SignUp() {
   const { isLoaded, signUp, setActive } = useSignUp()
-  const router = useRouter()
 
-  const [emailAddress, setEmailAddress] = React.useState('')
-  const [password, setPassword] = React.useState('')
+  const [data, setData] = React.useState({
+    email: '',
+    phone: '',
+    username: '',
+    password: ''
+  });
   const [pendingVerification, setPendingVerification] = React.useState(false)
-  const [code, setCode] = React.useState('')
+  const [showPassword, setShowPassword] = React.useState(false)
+  const handleChangeData = (field, value) => {
+    setData((prev) => ({
+      ...prev,
+      [field]: value
+    }));
+  };
+
 
   const onSignUpPress = async () => {
     if (!isLoaded) return
 
     try {
       await signUp.create({
-        emailAddress,
-        password,
+        emailAddress: data.email,
+        password: data.password,
       })
 
-    
+
       await signUp.prepareEmailAddressVerification({ strategy: 'email_code' })
 
       setPendingVerification(true)
     } catch (err) {
-    
+
       console.error(JSON.stringify(err, null, 2))
     }
   }
@@ -36,11 +46,10 @@ export default function SignUp() {
 
     try {
       const signUpAttempt = await signUp.attemptEmailAddressVerification({
-        code,
+        code: otp,
       })
       if (signUpAttempt.status === 'complete') {
         await setActive({ session: signUpAttempt.createdSessionId })
-        router.push('/Login')
       } else {
         console.error(JSON.stringify(signUpAttempt, null, 2))
       }
@@ -49,48 +58,181 @@ export default function SignUp() {
     }
   }
 
+  // otp 
+  const [otp, setOtp] = useState('');
+
   if (pendingVerification) {
     return (
-      <>
-        <Text>Verify your email</Text>
-        <TextInput
-          value={code}
-          placeholder="Enter your verification code"
-          onChangeText={(code) => setCode(code)}
-        />
-        <TouchableOpacity onPress={onVerifyPress}>
-          <Text>Verify</Text>
-        </TouchableOpacity>
-      </>
+      <KeyboardAvoidingView
+        style={styles.container}
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      >
+        <View style={{ minHeight: '30%', backgroundColor: '#ffffff04', width: '100%', alignItems: 'center', justifyContent: 'center' }}>
+          <Text style={{ color: '#efeae7ff', fontSize: 36, fontWeight: 'bold' }}>Verification</Text>
+          <Text style={{ color: '#efeae7ff', fontSize: 16, marginTop: 10 }}>We have send a code to your email</Text>
+        </View>
+        <View style={styles.containerInput}>
+          <View style={styles.inputContainer}>
+              <TextInput
+                value={otp}
+                placeholder="Enter your verification code"
+          onChangeText={(otp) => setOtp(otp)} 
+          style={styles.input}
+              />
+          </View>
+          <View style={{ padding: 10, position: "relative" }}>
+            <TouchableOpacity onPress={onVerifyPress} style={styles.buttonSubmit}>
+            <Text style={{ color: '#fffffffb', fontWeight: 'bold' }}>VERIFY</Text>
+          </TouchableOpacity>
+          </View>
+        </View>
+      </KeyboardAvoidingView>
     )
   }
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', padding: 20 }}>
-      <>
-        <Text>Sign up</Text>
-        <TextInput
-          autoCapitalize="none"
-          value={emailAddress}
-          placeholder="Enter email"
-          onChangeText={(email) => setEmailAddress(email)}
-        />
-        <TextInput
-          value={password}
-          placeholder="Enter password"
-          secureTextEntry={true}
-          onChangeText={(password) => setPassword(password)}
-        />
-        <TouchableOpacity onPress={onSignUpPress}>
-          <Text>Continue</Text>
-        </TouchableOpacity>
-        <View style={{ display: 'flex', flexDirection: 'row', gap: 3 }}>
-          <Text>Already have an account?</Text>
-          <Link href="/Login">
-            <Text>Sign in</Text>
-          </Link>
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      keyboardVerticalOffset={Platform.OS === 'ios' ? 0 : 0}
+    >
+      <ScrollView
+        contentContainerStyle={styles.scrollContent}
+        showsVerticalScrollIndicator={false}
+        keyboardShouldPersistTaps="handled"
+      >
+        <View style={styles.container}>
+          <View style={{ minHeight: '30%', backgroundColor: '#ffffff04', width: '100%', alignItems: 'center', justifyContent: 'center' }}>
+            <Text style={{ color: '#efeae7ff', fontSize: 36, fontWeight: 'bold' }}>Sign Up</Text>
+            <Text style={{ color: '#efeae7ff', fontSize: 16, marginTop: 10 }}>Please sign up to get started</Text>
+          </View>
+          <View style={styles.containerInput}>
+            <View style={styles.inputContainer}>
+              <Text style={{ color: '#000000c2', fontSize: 16, marginBottom: 8 }}>USERNAME</Text>
+              <TextInput
+                autoCapitalize="none"
+                value={data.username}
+                placeholder="Enter user name"
+                onChangeText={(text) => handleChangeData('username', text)}
+                style={styles.input}
+              />
+            </View>
+            <View style={styles.inputContainer} >
+              <Text style={{ color: '#000000c2', fontSize: 16, marginBottom: 8 }}>PHONE</Text>
+              <TextInput
+                autoCapitalize="none"
+                value={data.phone}
+                placeholder="Enter phone number"
+                onChangeText={(text) => handleChangeData('phone', text)}
+                style={styles.input}
+              />
+            </View>
+            <View style={styles.inputContainer}>
+              <Text style={{ color: '#000000c2', fontSize: 16, marginBottom: 8 }}>EMAIL</Text>
+              <TextInput
+                autoCapitalize="none"
+                value={data.email}
+                placeholder="Example@gmail.com"
+                onChangeText={(text) => handleChangeData('email', text)}
+                style={styles.input}
+              />
+            </View>
+            <View style={styles.inputContainer}>
+              <Text style={{ color: '#000000c2', fontSize: 16, marginBottom: 8 }}>PASSWORD</Text>
+              <View style={{ position: "relative", display: 'flex', justifyContent: 'space-between', justifyContent: 'center' }}>
+                <TextInput
+                  value={data.password}
+                  placeholder="Enter password"
+                  secureTextEntry={!showPassword}
+                  onChangeText={(text) => handleChangeData('password', text)}
+                  style={styles.input}
+                />
+                <TouchableOpacity style={styles.eyeButton} onPress={() => setShowPassword(!showPassword)}>
+                  <Ionicons name={showPassword ? "eye-outline" : "eye-off-outline"} size={24} style={{ color: '#0000008e' }} />
+                </TouchableOpacity>
+              </View>
+            </View>
+            <View style={{ padding: 10, position: "relative" }}>
+              <TouchableOpacity onPress={onSignUpPress} style={styles.buttonSubmit}>
+                <Text style={{ color: '#fffffffb', fontWeight: 'bold' }}>SIGNUP</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
         </View>
-      </>
-    </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ff6a00d9',
+  },
+  containerInput: {
+    minHeight: '100%',
+    backgroundColor: '#ffffffff',
+    width: '100%',
+    padding: 36,
+    borderTopLeftRadius: 36,
+    borderTopRightRadius: 36
+  },
+  inputContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    padding: 10,
+    position: "relative"
+  },
+  input: {
+    fontSize: 16,
+    color: '#000000c2',
+    paddingVertical: 16,
+    paddingHorizontal: 20,
+    backgroundColor: '#96a8be2f',
+    borderRadius: 12,
+    borderWidth: 1,
+    borderColor: '#ffffff',
+  },
+  buttonSubmit: {
+    backgroundColor: '#ff6a00d9',
+    borderRadius: 12,
+    paddingVertical: 16,
+    paddingHorizontal: 20,
+    alignItems: 'center',
+    borderColor: '#ffffffff',
+    borderWidth: 2,
+    width: '100%'
+  },
+  eyeButton: {
+    position: "absolute",
+    right: 5,
+    padding: 4,
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  scrollContent: {
+    flexGrow: 1,
+  },
+  otpContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+    marginBottom: 10,
+  }, otpInput: {
+    width: 50,
+    height: 50,
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 10,
+    textAlign: 'center',
+    fontSize: 20,
+  },
+  resend: {
+    marginBottom: 20,
+    color: 'gray',
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+  }
+});
