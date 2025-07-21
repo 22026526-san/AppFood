4aed475689562af16f2af7551f2ad1331032ff99
22026526-san <sangv6548@gmail.com>
2025-07-21 23:46:25 +0700

    backend api signup

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,12 @@
 import express from 'express';
-
+import './configs/database.js'; 
+import cors from 'cors';
 const app = express();
+import signupRoutes from './routes/signup.route.js';
+
+app.use(cors());
+app.use(express.json());
+app.use('/api/signup', signupRoutes);
 
 app.get("/api/appfood", (req, res) => {
   res.status(200).json({
---
+++ backend/src/controllers/signup.controller.js
@@ -0,0 +1,28 @@
+import pool from '../configs/database.js';
+import bcrypt from 'bcrypt';
+
+export const createUser = async (req, res) => {
+  try {
+    const { name, email, phone, password } = req.body;
+
+    if (!name || !email || !phone || !password) {
+      return res.status(400).json({ error: 'Missing required fields' });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const [result] = await pool.promise().query(
+      'INSERT INTO users (user_name, email, phone, password) VALUES (?, ?, ?, ?)',
+      [name, email, phone, hashedPassword]
+    );
+
+    res.status(201).json({
+      success: true,
+      user_id : result.insertId,
+    });
+  } catch (error) {
+    console.error('Error inserting user:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
